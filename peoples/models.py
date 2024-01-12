1fce01e472c20ecc15ee67194fbaeb1ddaeddda5
Hwang-Jaeryeong <jryeong67@naver.com>
2024-01-13 00:58:34 +0900

    [add] peoples 5개의질문이 있는데 마지막은 사진을 url로 입력받고 url 자체로 저장한 다음에 url 자체를 반환/ 로그인한 사용자마다 등록한 사람들이 다르기에 access token이용해서 리스트/등록 구분

---
+++ peoples/models.py
@@ -0,0 +1,17 @@
+# peoples/models.py
+
+from django.db import models
+from django.conf import settings
+
+
+class Person(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, blank=True, null=True)
+    name = models.CharField(max_length=255)
+    date = models.DateField()
+    exis = models.TextField()
+    motto = models.TextField()
+    introduction = models.TextField()
+    picture_url = models.URLField()
+
+    def __str__(self):
+        return self.name
---
+++ peoples/views.py
@@ -0,0 +1,23 @@
+# peoples/views.py
+
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from .models import Person
+from .serializers import PersonSerializer
+
+class PersonListCreateView(generics.ListCreateAPIView):
+    serializer_class = PersonSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Person.objects.filter(user=user)
+
+    def perform_create(self, serializer):
+        user = self.request.user
+        serializer.save(user=user)
+
+class PersonDetailView(generics.RetrieveAPIView):
+    queryset = Person.objects.all()
+    serializer_class = PersonSerializer
+    permission_classes = [IsAuthenticated]

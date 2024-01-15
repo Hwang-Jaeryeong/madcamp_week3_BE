9eb9681d6df96da69c7bbafc065adc3ed7e591d4
Hwang-Jaeryeong <jryeong67@naver.com>
2024-01-15 22:19:27 +0900

    image get 방식 변경

--- peoples/models.py
+++ peoples/models.py
@@ -2,7 +2,7 @@
 
 from django.db import models
 from django.conf import settings
-import uuid
+from PIL import Image
 
 
 class Person(models.Model):
@@ -12,7 +12,16 @@ class Person(models.Model):
     exis = models.TextField()
     motto = models.TextField()
     introduction = models.TextField()
-    picture_url = models.URLField()
+    picture_url = models.ImageField(upload_to='person_images/', null=True, blank=True)
 
     def __str__(self):
         return self.name
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        if self.picture_url:
+            img = Image.open(self.picture_url.path)
+            if img.height > 300 or img.width > 300:
+                output_size = (300, 300)
+                img.thumbnail(output_size)
+                img.save(self.picture_url.path)
--- peoples/views.py
+++ peoples/views.py
@@ -24,12 +24,19 @@ class PersonDetailView(generics.CreateAPIView):
     permission_classes = [IsAuthenticated]
 
     def create(self, request, *args, **kwargs):
-        # POST 요청으로 name을 전달받아 해당하는 Person 객체 정보를 반환합니다.
+        # POST 요청으로 name과 이미지를 전달받아 해당하는 Person 객체 정보를 반환합니다.
         name = self.request.data.get('name', None)
-        if name:
+        image = self.request.data.get('file', None)  # 'file' 키로부터 이미지를 가져옴
+
+        if name and image:
             person = Person.objects.filter(name=name, user=request.user).first()
             if person:
+                # 이미지 업로드 및 크기 조절 처리
+                person.picture_url = image
+                person.save()
+
                 serializer = self.get_serializer(person)
                 return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response({'error': 'Person not found.'}, status=status.HTTP_404_NOT_FOUND)
+
+        return Response({'error': 'Person not found or image not provided.'}, status=status.HTTP_404_NOT_FOUND)
 
